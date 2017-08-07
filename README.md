918ba8c95ac450e8cbbed42da15a5405b194d5ed
Hristiyan Dodov <h.dodov@gmail.com>
2017-08-07 16:25:11 +0300

    Version 1.0.0

---
+++ README.md
@@ -0,0 +1,37 @@
+# imagemin-instant
+
+Minify a folder of images recursively while preserving folder structure.
+
+# Installation
+
+First, make sure you've installed [Node.js](https://nodejs.org/) and [NPM](https://www.npmjs.com/). Then, run:
+
+```
+npm install -g imagemin-instant
+```
+
+# Usage
+
+Open a terminal and navigate to a folder with images.
+
+If you want to minify *all* images in the current directory, simply run:
+```
+imagemin
+```
+
+**Note: The original HD images will be overridden!**
+
+
+If you want to preserve the original images, you can specify an input and an output directory:
+
+```
+imagemin --in=my-images --out=minified-images
+```
+
+# Options
+
+```js
+--in=dir            // Specify an input directory.
+--out=dir           // Specify an output directory.
+--quality=50        // The quality of minified PNG files.
+```
---
+++ index.js
@@ -0,0 +1,61 @@
+const options = require("minimist")(process.argv.slice(2));
+const path = require("path");
+const recursive = require("recursive-readdir");
+const imagemin = require("imagemin");
+const imageminJpegtran = require("imagemin-jpegtran");
+const imageminPngquant = require("imagemin-pngquant");
+
+var ALLOWED_EXTENSIONS = [".png", ".jpg", ".jpeg"];
+var INPUT_DIR = "./";
+var OUTPUT_DIR = "./";
+var QUALITY  = 70;
+
+if (typeof options.quality === "number") {
+    QUALITY = options.quality;
+}
+
+if (options.in || options.out) {
+    if (!options.in) {
+        console.error("Error: Specify an input directory.");
+        process.exit(1);
+    } else if (!options.out) {
+        console.error("Error: Specify an output directory.");
+        process.exit(1);
+    } else {
+        INPUT_DIR = path.join(options.in);
+        OUTPUT_DIR = path.join(options.out);
+    }
+}
+
+recursive(INPUT_DIR, function (err, files) {
+    if (!Array.isArray(files)) {
+        console.error("No files found!");
+        process.exit(1);
+    }
+
+    files.forEach(function (file) {
+        var extenstion = path.extname(file).toLowerCase();
+
+        if (ALLOWED_EXTENSIONS.indexOf(extenstion) === -1) {
+            console.log("Skipped", file);
+            return false;
+        }
+
+        var fileParentDir = path.dirname(file);
+        var relativeToInputDir = path.relative(INPUT_DIR, fileParentDir);
+        var fileOutputDir = path.join(OUTPUT_DIR, relativeToInputDir);
+
+        imagemin([file], fileOutputDir, {
+            plugins: [
+                imageminJpegtran(),
+                imageminPngquant({
+                    quality: QUALITY
+                })
+            ]
+        }).then(function (files) {
+            files.forEach(function (minfile) {
+                console.log("Minified", minfile.path);
+            });
+        });
+    });
+});
